3e7e1b52e3093cef771270f997e2300f95cccc20
Birki <wolfgang.birkfellner@meduniwien.ac.at>
2017-01-29 14:03:49 +0100

    Add files via upload

--- sketch_handbox_TSC.ino
+++ sketch_handbox_TSC.ino
@@ -2,7 +2,8 @@
 int nSwitch = LOW; // move north button
 int eSwitch = LOW; 
 int sSwitch = LOW; 
-int wSwitch = LOW; 
+int wSwitch = LOW;
+int speedSwitch; 
 int switchStateChanged = 0;
 SoftwareSerial tscHB(10,11); //RX, TX
   
@@ -12,6 +13,12 @@ void setup() {
   pinMode(4,INPUT); // east
   pinMode(7,INPUT); // south
   pinMode(12,INPUT); // west
+  pinMode(13,INPUT); // speed selection switch
+  if (digitalRead(13) == LOW) {
+    speedSwitch=LOW;
+  } else {
+    speedSwitch=HIGH;
+  }
   Serial.begin(9600);
   while (!Serial) {}
   tscHB.begin(9600);
@@ -35,15 +42,21 @@ if (digitalRead(12) != wSwitch) {
   wSwitch = digitalRead(12);
   switchStateChanged=1;
 }
+if (digitalRead(13) != speedSwitch) {
+  speedSwitch = digitalRead(12);
+  switchStateChanged=1;
+}
 if (switchStateChanged == 1) {
   Serial.print(nSwitch);
   Serial.print(eSwitch);
   Serial.print(sSwitch);
-  Serial.println(wSwitch);
+  Serial.print(wSwitch);
+  Serial.println(speedSwitch);
   tscHB.print(nSwitch);
   tscHB.print(eSwitch);
   tscHB.print(sSwitch);
-  tscHB.println(wSwitch);
+  tscHB.print(wSwitch);
+  tscHB.println(speedSwitch);
 }
 delay(50);
 switchStateChanged=0;
