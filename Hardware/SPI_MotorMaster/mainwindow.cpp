66b197441589e670d82e7127c6a2bcfe98d76f67
Birki <wolfgang.birkfellner@meduniwien.ac.at>
2017-10-19 14:15:53 +0000

    further work on SPI checker

--- Hardware/SPI_MotorMaster/mainwindow.cpp
+++ Hardware/SPI_MotorMaster/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
 
 //-------------------------------------------------------------------------------------
 
@@ -24,6 +25,18 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
     } else {
         this->commParams.selectedChannel = 1;
     }
+    this->timer = new QTimer();
+    this->timer->setInterval(100);
+    this->timer->start();
+    connect(ui->pbSendKinematics, SIGNAL(clicked()), this, SLOT(sendKinematics()));
+    connect(ui->pbStart, SIGNAL(clicked()), this, SLOT(startDrive()));
+    connect(ui->pbStop, SIGNAL(clicked()), this, SLOT(stopDrive()));
+    connect(ui->pbExit, SIGNAL(clicked()), this, SLOT(terminateProgram()));
+    connect(ui->rbSPI0, SIGNAL(released()), this, SLOT(switchChannel()));
+    connect(ui->rbSPI1, SIGNAL(released()), this, SLOT(switchChannel()));
+    connect(this->timer, SIGNAL(timeout()), this, SLOT(checkDrivesForActivity()));
+    this->commParams.guiData = new QString("t"); // query type of driver ... to be completed
+
 }
 
 //--------------------------------------------------------------------------------------
@@ -34,6 +47,192 @@ MainWindow::~MainWindow() {
 
 //--------------------------------------------------------------------------------------
 
-void MainWindow::enableDrive(void) {
+void MainWindow::checkDrivesForActivity(void) {
+    this->commParams.guiData->clear();
+    this->commParams.guiData->append("d0");
+    qDebug() << this->commParams.guiData->toLatin1();
+    this->commParams.guiData->clear();
+    this->commParams.guiData->append("d1");
+    qDebug() << this->commParams.guiData->toLatin1();
+}
 
+//--------------------------------------------------------------------------------------
+
+void MainWindow::terminateProgram(void) {
+    this->enableDrive(0,0);
+    this->enableDrive(1,0);
+    exit(0);
+}
+
+//--------------------------------------------------------------------------------------
+
+void MainWindow::switchChannel(void) {
+    if (ui->rbSPI0->isChecked()) {
+        this->commParams.selectedChannel = 0;
+    } else {
+        this->commParams.selectedChannel = 1;
+    }
+    qDebug() << this->commParams.selectedChannel;
+}
+
+//--------------------------------------------------------------------------------------
+
+void MainWindow::getAcc(void) {
+    int val;
+    int driveAddressed;
+
+    val = ui->sbAcc->value();
+    if (ui->rbKinematics1->isChecked()) {
+        driveAddressed = 0;
+    } else {
+        driveAddressed = 1;
+    }
+    this->commParams.guiData->clear();
+    this->commParams.guiData->append("a");
+    this->commParams.guiData->append(QString::number(driveAddressed));
+    this->commParams.guiData->append(QString::number(val));
+    qDebug() << this->commParams.guiData->toLatin1();
+}
+
+//--------------------------------------------------------------------------------------
+
+void MainWindow::getVel(void) {
+    int val;
+    int driveAddressed;
+
+    val = ui->sbVel->value();
+    if (ui->rbKinematics1->isChecked()) {
+        driveAddressed = 0;
+    } else {
+        driveAddressed = 1;
+    }
+    this->commParams.guiData->clear();
+    this->commParams.guiData->append("v");
+    this->commParams.guiData->append(QString::number(driveAddressed));
+    this->commParams.guiData->append(QString::number(val));
+    qDebug() << this->commParams.guiData->toLatin1();
+}
+
+//--------------------------------------------------------------------------------------
+
+void MainWindow::getSteps(void) {
+    int val;
+    int driveAddressed;
+
+    val = ui->sbSteps->value();
+    if (ui->rbKinematics1->isChecked()) {
+        driveAddressed = 0;
+    } else {
+        driveAddressed = 1;
+    }
+    this->commParams.guiData->clear();
+    this->commParams.guiData->append("s");
+    this->commParams.guiData->append(QString::number(driveAddressed));
+    this->commParams.guiData->append(QString::number(val));
+    qDebug() << this->commParams.guiData->toLatin1();
+}
+//--------------------------------------------------------------------------------------
+
+void MainWindow::getMSteps(void) {
+    QString *val;
+
+    val = new QString("m ");
+    if (ui->rbFullStep->isChecked()) {
+        val->append("001");
+    }
+    if (ui->rbHalfStep->isChecked()) {
+        val->append("002");
+    }
+    if (ui->rbQuarterStep->isChecked()) {
+        val->append("004");
+    }
+    if (ui->rbEigthStep->isChecked()) {
+        val->append("008");
+    }
+    if (ui->rbSixteenthStep->isChecked()) {
+        val->append("016");
+    }
+    if (ui->rb32thStep->isChecked()) {
+        val->append("032");
+    }
+    if (ui->rb64thStep->isChecked()) {
+        val->append("064");
+    }
+    if (ui->rb128thStep->isChecked()) {
+        val->append("128");
+    }
+    this->commParams.guiData->clear();
+    this->commParams.guiData->append(val);
+    delete val;
+    qDebug() << this->commParams.guiData->toLatin1();
+}
+
+
+//--------------------------------------------------------------------------------------
+
+void MainWindow::enableDrive(short which, short enable) {
+
+    if (which < 0) {
+        which = 0;
+    }
+    if (which > 1) {
+        which = 1;
+    }
+    this->commParams.guiData->clear();
+    this->commParams.guiData->append("e");
+    this->commParams.guiData->append(QString::number(which));
+    if (enable == 0) {
+        this->commParams.guiData->append("0");
+    } else {
+        this->commParams.guiData->append("1");
+    }
+    if (which == 0) {
+        ui->cbEnableDrive1->setChecked(enable);
+    } else {
+        ui->cbEnableDrive2->setChecked(enable);
+    }
+    qDebug() << this->commParams.guiData->toLatin1();
+}
+
+//--------------------------------------------------------------------------------------
+
+void MainWindow::startDrive(void) {
+
+
+    if (ui->rbCtrlDrive1->isChecked()) {
+        this->enableDrive(0,1);
+        this->commParams.guiData->clear();
+        this->commParams.guiData->append("o0");
+    } else {
+        this->enableDrive(1,1);
+        this->commParams.guiData->clear();
+        this->commParams.guiData->append("o1");
+    }
+    qDebug() << this->commParams.guiData->toLatin1();
+}
+
+//--------------------------------------------------------------------------------------
+
+void MainWindow::stopDrive(void) {
+
+    this->commParams.guiData->clear();
+    if (ui->rbCtrlDrive1->isChecked()) {
+        this->commParams.guiData->append("x0");
+        qDebug() << this->commParams.guiData->toLatin1();
+        this->enableDrive(0,0);
+    } else {
+        this->commParams.guiData->append("x1");
+        qDebug() << this->commParams.guiData->toLatin1();
+        this->enableDrive(1,0);
+    }
+
+}
+
+//--------------------------------------------------------------------------------------
+
+void MainWindow::sendKinematics(void) {
+    this->getAcc();
+    this->getVel();
+    this->getSteps();
+    this->getMSteps();
 }
--- Hardware/SPI_MotorMaster/mainwindow.h
+++ Hardware/SPI_MotorMaster/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTimer>
 #include "spi_drive.h"
 
 namespace Ui {
@@ -15,20 +16,32 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    void getAcc(void);
+    void getVel(void);
+    void getSteps(void);
+    void enableDrive(short, short);
+    void getMSteps(void);
 
 private:
     Ui::MainWindow *ui;
+    QTimer *timer;
     SPI_Drive *spiDrOnChan0;
     SPI_Drive *spiDrOnChan1;
     struct currentCommunicationParameters {
         bool chan0IsOpen;
         bool chan1IsOpen;
         int selectedChannel;
+        QString *guiData;
     };
     currentCommunicationParameters commParams;
 
 private slots:
-    void enableDrive(void);
+    void startDrive(void);
+    void stopDrive(void);
+    void sendKinematics(void);
+    void terminateProgram(void);
+    void switchChannel(void);
+    void checkDrivesForActivity(void);
 };
 
 #endif // MAINWINDOW_H
