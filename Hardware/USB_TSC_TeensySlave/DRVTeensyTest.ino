9ee0d56704e06dea121c7aa8e6dd7a7592296604
Birki <wolfgang.birkfellner@meduniwien.ac.at>
2017-12-19 15:07:43 +0100

    adopted teensy test program for v. 1,.1

--- Hardware/USB_TSC_TeensySlave/DRVTeensyTest.ino
+++ Hardware/USB_TSC_TeensySlave/DRVTeensyTest.ino
@@ -3,10 +3,12 @@
 AccelStepper deStepper(AccelStepper::DRIVER,A4,A5);
 AccelStepper raStepper(AccelStepper::DRIVER,A7,A8);
 AccelStepper aux1Stepper(AccelStepper::DRIVER,A21,A22);
-AccelStepper aux2Stepper(AccelStepper::DRIVER,A18,A19);
+AccelStepper aux2Stepper(AccelStepper::DRIVER,A13,A12);
 
 
 void setup() {
+   Serial.begin(115200);
+
    pinMode(A0,OUTPUT);// enable de
    digitalWrite(A0,HIGH);
    pinMode(A6,OUTPUT);// enable ra
@@ -30,26 +32,26 @@ void setup() {
    digitalWrite(A16,HIGH);
    digitalWrite(A17,LOW); // LLL=full, HLL=half,LHL=1/4,HHL=1/8,LLH=1/16,HLH=LHH=HHH=1/32 for the DRV8825    
 
-   deStepper.setMaxSpeed(3000); 
-   deStepper.setAcceleration(5000); 
+   deStepper.setMaxSpeed(2000); 
+   deStepper.setAcceleration(2000); 
    deStepper.setCurrentPosition(0);
    digitalWrite(A0,LOW);
    deStepper.moveTo(50000);
    
-   raStepper.setMaxSpeed(3000); 
-   raStepper.setAcceleration(5000); 
+   raStepper.setMaxSpeed(2000); 
+   raStepper.setAcceleration(2000); 
    raStepper.setCurrentPosition(0);
    digitalWrite(A6,LOW);
    raStepper.moveTo(50000);
    
-   aux1Stepper.setMaxSpeed(3000); 
-   aux1Stepper.setAcceleration(5000); 
+   aux1Stepper.setMaxSpeed(2000); 
+   aux1Stepper.setAcceleration(2000); 
    aux1Stepper.setCurrentPosition(0);
    digitalWrite(A14,LOW);
    aux1Stepper.moveTo(50000);
    
-   aux2Stepper.setMaxSpeed(3000); 
-   aux2Stepper.setAcceleration(5000); 
+   aux2Stepper.setMaxSpeed(2000); 
+   aux2Stepper.setAcceleration(2000); 
    aux2Stepper.setCurrentPosition(0);
    digitalWrite(A20,LOW);
    aux2Stepper.moveTo(50000);
@@ -64,11 +66,12 @@ void loop() {
      digitalWrite(A20,HIGH);  
   }
   if (aux1Stepper.isRunning() == false) {
-     digitalWrite(A14,HIGH);  
+     digitalWrite(A14,HIGH);
+     Serial.println("A14 is HIGH");  
   }
     if (deStepper.isRunning() == false) {
      digitalWrite(A0,HIGH);  
-  }
+  } 
   if (raStepper.isRunning() == false) {
      digitalWrite(A6,HIGH);  
   }
